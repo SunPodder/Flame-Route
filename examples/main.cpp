5c5f562511be5cda776ba2729d0c2756d019b527
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-03-21 20:18:28 +0600

    migrate examples

---
+++ examples/main.cpp
@@ -0,0 +1,20 @@
+#include <http/method.hpp>
+#include <http/request.hpp>
+#include <http/response.hpp>
+#include <http/server.hpp>
+
+void home(const HTTPRequest &request, HTTPResponse &response){
+    response.body = "<h1>Welcome to Home</h1>";
+}
+
+int main(){
+    FlameServer *server = new FlameServer();
+
+    server->register_static_route("/static", "./");
+
+    server->register_route("/", {GET}, home);
+
+    server->ignite("127.0.0.1", 8080);
+
+    return 0;
+}
