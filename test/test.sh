d7abb158f510f9bfa594a16d896d9d8f67556603
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-03-31 21:09:16 +0600

    update test

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-$SCRIPT_DIR/../build/examples/example &
+ROOT_DIR=$(dirname $SCRIPT_DIR)
+cd $ROOT_DIR/examples
+$ROOT_DIR/build/examples/example &
 sleep 1
 
-curl localhost:8080 | grep "Welcome to Home" || exit 1
+curl localhost:8080 | grep "Hello Flame!" || exit 1
 
 echo "Passed! ✅"
 killall example
