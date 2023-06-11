3452764ef48c281365627b44c9e9a4ee182e7d5a
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-06-11 22:59:26 +0600

    fix include-what-you-see include dir

--- check.sh
+++ check.sh
@@ -6,6 +6,6 @@ if ! [[ $(command -v include-what-you-use && command -v cppcheck) ]]; then
 fi
 
 for file in $(find ./src -name *.cpp | grep -v mime); do
-    include-what-you-use $file
+    include-what-you-use -I./src $file
     cppcheck $file
 done
