adf5e2c8394ed498aeaf0ea28e28f4d33f4ff2e2
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-06-12 09:47:47 +0600

    update check.sh

--- check.sh
+++ check.sh
@@ -5,7 +5,24 @@ if ! [[ $(command -v include-what-you-use && command -v cppcheck) ]]; then
     exit
 fi
 
-for file in $(find ./src -name *.cpp | grep -v mime); do
+# list all changed .cpp and .hpp files
+changed_files=$(git diff --name-only HEAD~1 | grep -E "\.(cpp|hpp)$")
+
+if [[ -z "$changed_files" ]]; then
+	exit
+else
+	echo "Changed files:"
+	echo "$changed_files"
+fi
+
+
+echo "Ignore \`<sstream>\` include error of \`src/http/request.cpp\`. Don't remove that header."
+echo ""
+
+for file in $changed_files; do
     include-what-you-use -I./src $file
     cppcheck $file
 done
+
+printf "\n\n\n"
+
