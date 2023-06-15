fc297a41a47b9302a3c7a7ee34d1de0086237d32
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-06-15 09:06:42 +0600

    minor enhancements

--- src/http/server.cpp
+++ src/http/server.cpp
@@ -1,19 +1,17 @@
+#include <http/method.hpp>
+#include <http/route.hpp>
 #include <http/server.hpp>
 #include <http/utils.hpp>
 #include <iostream>
 #include <logger/logger.hpp>
+#include <socket/socket.hpp>
 #include <string>
 #include <thread>
 #include <unordered_map>
-#include <http/method.hpp>
-#include <http/route.hpp>
-#include <socket/socket.hpp>
 class HTTPRequest;
 class HTTPResponse;
 
-
-FlameServer::FlameServer() {
-}
+FlameServer::FlameServer() {}
 
 int FlameServer::ignite(std::string ip_addr, int port, void (*callback)()) {
 	socket = new Socket();
@@ -21,60 +19,62 @@ int FlameServer::ignite(std::string ip_addr, int port, void (*callback)()) {
 	(void)socket->bind(ip_addr, port);
 	(void)socket->listen(10);
 
-    if (callback != nullptr) {
-        callback();
-    }else{
-        Logger::Success("Server started on " + ip_addr + ":" + std::to_string(port));
-    }
+	if (callback != nullptr) {
+		callback();
+	} else {
+		Logger::Success("Server started on " + ip_addr + ":" +
+						std::to_string(port));
+	}
 
-    // keep accepting connections
-    // TODO: limit number of max connections
-    // push connections to a queue if limit is reached
-    // and process them later
-    // might also accept connections in a seperate thread?
-    while (true) {
-        Socket *client = socket->accept();
-		
-        if (client->getFd() < 0) {
+	// keep accepting connections
+	// TODO: limit number of max connections
+	// push connections to a queue if limit is reached
+	// and process them later
+	// might also accept connections in a seperate thread?
+	while (true) {
+		Socket* client = socket->accept();
+
+		if (client->getFd() < 0) {
 			Logger::Info("Failed to accept client");
 			delete client;
-            continue;
-        }
-		
-		struct thread_args *args = new struct thread_args(*this, client);	
+			continue;
+		}
+
+		struct thread_args* args = new struct thread_args(*this, client);
 		std::thread t(handle_connection, args);
 		t.detach();
-    }
+	}
 }
-
 
 void FlameServer::route(std::string path, const HTTPMethod (&method)[9],
-        void (*callback)(const HTTPRequest &request, HTTPResponse &response)) {
-    Route *route = new Route();
-    route->path = path;
-    for (int i = 0; i < (int)(sizeof(method) / sizeof(HTTPMethod)); i++) {
-        route->methods[i] = method[i];
-    }
-    route->callback = callback;
-    this->routes.insert({ path, *route });
-    this->_route_count++;
+						void (*callback)(const HTTPRequest& request,
+										 HTTPResponse& response)) {
+	Route* route = new Route();
+	route->path = path;
+	for (int i = 0; i < (int)(sizeof(method) / sizeof(HTTPMethod)); i++) {
+		route->methods[i] = method[i];
+	}
+	route->callback = callback;
+	this->routes.insert({path, *route});
+	this->_route_count++;
 }
 
-
-void FlameServer::static_route(std::string path){
-    this->static_routes.insert({ path, path });
-    this->_static_route_count++;
+void FlameServer::static_route(std::string path) {
+	this->static_routes.insert({path, path});
+	this->_static_route_count++;
 }
 
 void FlameServer::static_route(std::string path, std::string file_path) {
-    this->static_routes.insert({ path, file_path });
-    this->_static_route_count++;
+	this->static_routes.insert({path, file_path});
+	this->_static_route_count++;
 }
 
 std::string FlameServer::__str__() {
-    std::string str = "<FlameServer - " + std::to_string(this->_route_count) + " routes, " + std::to_string(this->_static_route_count) + " static routes>";
-   
-    return str;
+	std::string str = "<FlameServer - " + std::to_string(this->_route_count) +
+					  " routes, " + std::to_string(this->_static_route_count) +
+					  " static routes>";
+
+	return str;
 }
 
 FlameServer::~FlameServer() {
@@ -85,4 +85,3 @@ FlameServer::~FlameServer() {
 	this->routes.clear();
 	this->static_routes.clear();
 }
-
