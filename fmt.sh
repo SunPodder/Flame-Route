ec230c9816ee89d9d124c67d6e80942852577ed0
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-06-21 14:31:47 +0600

    fix pre-commit formater

--- fmt.sh
+++ fmt.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-# get all changed files
-files=$(git diff --name-only HEAD | grep -E '\.(cpp|hpp)$' | grep -v 'src/mime' | grep -v "json_impl" | grep -v "inja")
+# if arguments are given format them
+if [[ ! -z "$1" ]]; then
+	files=$@
+else
+	# get all changed files
+	files=$(git diff --name-only HEAD | grep -E '\.(cpp|hpp)$' | grep -v 'src/mime' | grep -v "json_impl" | grep -v "inja")
+fi
 
-# format all changed files
 for file in $files; do
 	clang-format -i $file
 done
