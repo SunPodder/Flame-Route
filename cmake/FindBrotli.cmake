92434bf38e446704e4bb767377301aab5e978bb6
Sunlight <73415680+SunPodder@users.noreply.github.com>
2023-06-27 10:45:30 +0600

    fix symbols missing in global lib

--- cmake/FindBrotli.cmake
+++ cmake/FindBrotli.cmake
@@ -1,14 +1,17 @@
 find_path(BROTLI_INCLUDES NAMES brotli/encode.h brotli/decode.h)
-find_library(BROTLI_ENC NAMES brotlienc)
-find_library(BROTLI_DEC NAMES brotlidec)
 
-if(BROTLI_INCLUDES AND BROTLI_ENC AND BROTLI_DEC)
+set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+find_library(BROTLI_ENC NAMES brotlienc brotlienc-static)
+find_library(BROTLI_DEC NAMES brotlidec brotlidec-static)
+find_library(BROTLI_COMMON NAMES brotlicommon brotlicommon-static)
+
+if(BROTLI_INCLUDES AND BROTLI_ENC AND BROTLI_DEC AND BROTLI_COMMON)
 	set(BROTLI_FOUND TRUE)
 endif()
 
 if(BROTLI_FOUND)
 	if(NOT BROTLI_FIND_QUIETLY)
-		message(STATUS "Found Brotli: ${BROTLI_LIBRARIES}")
+		message(STATUS "Found Brotli:\n- Includes: ${BROTLI_INCLUDES}\n- Libraries: ${BROTLI_ENC} ${BROTLI_DEC} ${BROTLI_COMMON}")
 	endif()
 else()
 	if(BROTLI_FIND_REQUIRED)
@@ -16,13 +19,18 @@ else()
 	endif()
 endif()
 
-add_library(Brotli::ENC INTERFACE IMPORTED)
+add_library(Brotli::ENC STATIC IMPORTED GLOBAL)
 set_target_properties(Brotli::ENC PROPERTIES
-	INTERFACE_LINK_LIBRARIES "${BROTLI_ENC}"
+	IMPORTED_LOCATION "${BROTLI_ENC}"
 )
 
-add_library(Brotli::DEC INTERFACE IMPORTED)
+add_library(Brotli::DEC STATIC IMPORTED GLOBAL)
 set_target_properties(Brotli::DEC PROPERTIES
-	INTERFACE_LINK_LIBRARIES "${BROTLI_DEC}"
+	IMPORTED_LOCATION "${BROTLI_DEC}"
+)
+
+add_library(Brotli::COMMON STATIC IMPORTED GLOBAL)
+set_target_properties(Brotli::COMMON PROPERTIES
+	IMPORTED_LOCATION "${BROTLI_COMMON}"
 )
 
